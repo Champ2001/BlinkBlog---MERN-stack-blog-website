bcebb3acac5798862a0af44816849128d3c773bc
Sarnava Mohanta <sarnavamohanta2001@gmail.com>
2024-04-18 18:14:55 +0530

    Made changes to categories

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,30 +1,34 @@
 export default function About() {
   return (
-    <div className='min-h-screen flex items-center justify-center'>
-      <div className='max-w-2xl mx-auto p-3 text-center'>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
         <div>
-          <h1 className='text-3xl font font-semibold text-center my-7'>
-            About Sarnava's Blog
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About CampusHub
           </h1>
-          <div className='text-md text-gray-500 flex flex-col gap-6'>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
             <p>
-              Welcome to Sarnava's Blog! This blog was created by Sarnava Mohanta
-              as a personal project to share my thoughts and ideas with the
-              world. I am a passionate developer who loves to write about
-              technology, coding, and everything in between.
+              In today’s digital age, university students face significant
+              challenges in terms of community resources and campus life. This
+              study supports CampusHub, which aims to address these issues.
+              CampusHub seamlessly integrates in-store assessment with
+              community-driven problem solving techniques, providing
+              comprehensive solutions to improve the student experience.
             </p>
 
             <p>
-              On this blog, you'll find weekly articles and tutorials on topics
-              such as web development, software engineering, and programming
-              languages.
+              The methodology adopts a holistic approach, refining the features
+              through iterative processes guided by user feedback. Additionally,
+              the study suggests incorporating on-campus food delivery services,
+              to provide convenient dining options.
             </p>
 
             <p>
-              We encourage you to leave comments on our posts and engage with
-              other readers. You can like other people's comments and reply to
-              them as well. We believe that a community of learners can help
-              each other grow and improve.
+              The findings highlight how effective the CampusHub is in
+              addressing student challenges, fostering engagement and bridging
+              information gaps. These exchange programs are emerging as an
+              important tool for universities to increase student support and
+              community engagement.
             </p>
           </div>
         </div>
